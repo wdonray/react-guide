64d6d02edd60469be2ead50ea97c21fefc4ce7f1
Donray Williams <donray_williams@gemvision.com>
2019-12-26 16:35:42 -0600

    In Progress

--- src/Guide/GuideController.js
+++ src/Guide/GuideController.js
@@ -1,16 +1,36 @@
-export function Step(element, content) {
+export function Step(element, content, contentPosition) {
     this.element = element;
     this.content = content;
+    this.contentPosition = contentPosition;
 }
+export const ContentPosition =  {
+    topLeft: 1,
+    topMiddle: 2,
+    topRight: 3,
+    middleLeft: 4,
+    middle: 5,
+    middleRight: 6,
+    bottomLeft: 7,
+    bottomMiddle: 8,
+    bottomRight: 9
+};
+
 export class GuideController {
-    constructor(steps, currentStep, prevStep) {
+    constructor(active, steps, currentStep, prevStep) {
+        this.active = active;
         this.steps = steps;
         this.currentStep = currentStep;
         this.prevStep = prevStep;
     }
+    //Active
+    getActive = () => this.active;
+    setActive = (state) => this.active = state;
     //Steps
     getSteps = () => this.steps;
-    addStep = (step) => this.steps.push(step);
+    addStep = (step) => {
+        this.steps.push(step);
+        this.currentStep = this.steps[this.steps.length - 1];
+    };
     getCurrentStep = () => this.currentStep;
     gotToStep = (step) => {
         let found = this.steps.find(item => item === step);
@@ -21,8 +41,8 @@ export class GuideController {
         return this.currentStep;
     };
     removeStep = (step) => {
-        for(let i = 0; i < this.steps.length; i++){
-            if ( this.steps[i] === step) {
+        for (let i = 0; i < this.steps.length; i++) {
+            if (this.steps[i] === step) {
                 this.steps.splice(i, 1);
             }
         }
--- src/Guide/GuideRenderer.js
+++ src/Guide/GuideRenderer.js
@@ -1,24 +1,105 @@
-import React, { Component } from 'react';
-import {GuideController, Step} from "./GuideController";
+import React, {Component} from 'react';
+import {ContentPosition, GuideController, Step} from "./GuideController";
+
+//TODO: Jumping Content / Non-clickable area / Finished Content Holder / Direction of steps? 
 
 class GuideRenderer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            guide: new GuideController([], null, null)
-        }
+            guide: new GuideController(true, [], null, null),
+            currentStep: null,
+            active: true,
+        };
     }
 
-    render(){
+    //TESTING
+    componentWillMount() {
+        this.setState({active: this.state.guide.getActive()});
+        this.state.guide.addStep(new Step('test1', 'This is test 1', ContentPosition.middle));
+        this.state.guide.addStep(new Step('test2', 'This is test 2', ContentPosition.bottomLeft));
+        console.log({BeforeRender: document.getElementById(this.state.guide.getCurrentStep().element)})
+    }
 
+    componentDidMount() {
+        console.log(this.state.guide.getCurrentStep());
+        this.setState({currentStep: this.state.guide.getCurrentStep()}, () => console.log({AfterRender: document.getElementById(this.state.currentStep.element)}));
+    }
+
+    contentPosition = (contentPosition) => {
+        switch (contentPosition) {
+            case ContentPosition.middle:
+                return {
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%'
+                };
+            case ContentPosition.bottomLeft:
+                return {
+                    position: 'absolute',
+                    bottom: '8px',
+                    left: '16px'
+                };
+            default:
+                return {};
+        }
+    };
+
+    render() {
         return <div>
-            <button id={'test'} onClick={() => {
-                this.state.guide.addStep(new Step('test', 'This is test'))
-                console.log(this.state.guide.getSteps())
+            <button id={'test1'} style={{width: '100px', height: '300px'}} onClick={() => {
+                this.state.guide.goToPrevStep();
+                this.setState({currentStep: this.state.guide.getCurrentStep()});
             }}>
-                Add Step
+                Prev Step
             </button>
+            <button style={{width: '200px', marginLeft: '30px'}} onClick={() => {
+                this.state.guide.nextStep();
+                this.setState({currentStep: this.state.guide.getCurrentStep()});
+            }} id={'test2'}>
+                Next Step
+            </button>
+
+            <button style={{marginLeft: '30px', zIndex: '9999', position: 'fixed', backgroundColor: 'white'}}
+                    onClick={() => {
+                        this.state.guide.setActive(!this.state.guide.getActive());
+                        this.setState({active: this.state.guide.getActive()});
+                    }}>
+                Toggle Active
+            </button>
+            {/* DIMMER */}
+            {
+                (this.state.guide.getActive() && this.state.currentStep && document.getElementById(this.state.currentStep.element) !== null) ?
+                    <div>
+                        <div style={{
+                            position: "fixed",
+                            width: document.getElementById(this.state.currentStep.element).offsetWidth,
+                            height: document.getElementById(this.state.currentStep.element).offsetHeight,
+                            top: document.getElementById(this.state.currentStep.element).offsetTop - 10,
+                            left: document.getElementById(this.state.currentStep.element).offsetLeft - 10,
+                            boxShadow: '0 0 0 100vmax rgba(0, 0, 0, .6)',
+                            // border: '1px solid #777777',
+                            // borderRadius: '5px',
+                            pointerEvents: 'none',
+                            padding: '10px',
+                            transition: 'all 0.5s',
+                        }}/>
+                        <div style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            // minHeight: '100vh',
+                            zIndex: '9999',
+                            position: 'fixed',
+                            backgroundColor: 'white',
+                            transition: 'all 1s',
+                            ...this.contentPosition(this.state.currentStep.contentPosition)
+                        }}>
+                            {this.state.currentStep.content}
+                        </div>
+                    </div> : null
+            }
         </div>
     };
 }
+
 export default GuideRenderer
